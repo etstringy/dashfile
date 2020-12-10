dee3fa2871e1469cdbbe20aa4f1dd4ed343e65fb
etstringy <eddyteddyt@gmail.com>
2020-12-10 20:09:14 +0000

    AAAAAA

--- frontend/communicator.js
+++ frontend/communicator.js
@@ -207,3 +207,11 @@ String.prototype.toHHMMSS = function () {
   }
   return hours + ":" + minutes + ":" + seconds;
 };
+
+document.addEventListener("keydown", function (e) {
+  if (e.which === 123) {
+    require("remote").getCurrentWindow().toggleDevTools();
+  } else if (e.which === 116) {
+    location.reload();
+  }
+});
